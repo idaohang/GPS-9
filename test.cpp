b5d2cc224ac59d57db964b57fb26faa32bb3f2ac
Lemko <cedukr@hotmail.fr>
2014-04-04 19:14:33 +0200

    Signed-off-by: Lemko <cedukr@hotmail.fr>

--- test.cpp
+++ test.cpp
@@ -331,14 +331,18 @@ double rad2deg(double rad) {
       }
     }
     // op []
-    CCoord operator[](unsigned int i)
+    CCoord operator[](unsigned int i1)
     {
+      if (i1<0) throw string("error");
+      unsigned int i=i1;
       if (i>table.size()-1 /*|| i<0*/)     throw string("error");
       return table[i];
     }
     // op ()
-    CGPS operator()(unsigned int a,unsigned int b)
+    CGPS operator()( int a1,int b1)
     {
+      if (a1<0 || b1<0) throw string ("error");
+      unsigned int a=a1; unsigned int b=b1;
       if (a>table.size()-1 /*|| a<0 || b<0 */|| b>table.size()-1)     throw string("error");
       CGPS x0;
       for (unsigned int i=a;i<=b;i++)
@@ -458,12 +462,13 @@ double rad2deg(double rad) {
     {
     string str=rhs.gulp(is);
     vector<CCoord> v;
-    cout<<str<<endl;
     bool test= rhs.testeur(str, v);
     if (test)
     {
+      CGPS nouveau;
       for (unsigned int i=0;i<v.size();i++)
-        rhs . Add (v[i]);
+        nouveau . Add (v[i]);
+      rhs=nouveau;
     }
     else
     {
@@ -480,17 +485,101 @@ double rad2deg(double rad) {
 
  };
 
-/*
+
 
 int main()
 {
 double d;
 CGPS x0;
+cout << x0;
+ // []
+
+x0 += "(50 6\'16.5\"N, 14 23\'20.25\" E)";
+cout << x0;
+ // [(50 6'16.5"N, 14 23'20.25"E)]
+
+x0 . Add ( CCoord ( "(50.1003781N, 14.3925125E)" ) ) . Add ( CCoord ( 50.084202, 14.423357 ) );
+cout << x0;
+ // [(50 6'16.5"N, 14 23'20.25"E) > (50 6'1.361"N, 14 23'33.045"E) > (50 5'3.127"N, 14 25'24.085"E)]
+
+d = x0 . Distance ();
+ // d = 3373.886483
+
+x0 += "(50.0843122N, 14.417463 E)";
+cout << x0;
+ // [(50 6'16.5"N, 14 23'20.25"E) > (50 6'1.361"N, 14 23'33.045"E) > (50 5'3.127"N, 14 25'24.085"E) > (50 5'3.524"N, 14 25'2.867"E)]
+
+cout << x0;
+ // [(50 6'16.5"N, 14 23'20.25"E) > (50 6'1.361"N, 14 23'33.045"E) > (50 5'3.127"N, 14 25'24.085"E) > (50 5'3.524"N, 14 25'2.867"E)]
+
+//d = x0 . Distance ();
+ // d = 3794.598230
+
+
+CCoord tmp0 = x0 [ 1 ];
+cout << tmp0;
+ // (50 6'1.361"N, 14 23'33.045"E)
+
+CGPS tmp1 = x0 ( 1, 2 );
+cout << tmp1;
+ // [(50 6'1.361"N, 14 23'33.045"E) > (50 5'3.127"N, 14 25'24.085"E)]
+
+
 CGPS x1;
-istringstream tmp4 ( "[(51.5N, 0.0E) > (33.9S, 151.2E) > (37.42190N, 122.08405W) > (10.00N, 50.000W)]" );
+istringstream tmp2 ( "[(50 6\'16.5\"N, 14 23\'20.25\" E) > (51.5N, 0.0E) > (33.9S, 151.2E) > (37.42190N, 122.08405W) > (0.00N, 50.000W)]" );
+tmp2 >> x1;
+ // tmp2 . fail () = false
+
+
+cout << x1;
+ // [(50 6'16.5"N, 14 23'20.25"E) > (51 30'0"N, 0 0'0"E) > (33 54'0"S, 151 12'0"E) > (37 25'18.84"N, 122 5'2.58"W) > (0 0'0"N, 50 0'0"W)]
+
+cout << x1;
+ // [(50 6'16.5"N, 14 23'20.25"E) > (51 30'0"N, 0 0'0"E) > (33 54'0"S, 151 12'0"E) > (37 25'18.84"N, 122 5'2.58"W) > (0 0'0"N, 50 0'0"W)]
+
+d = x1 . Distance ();
+ // d = 38401662.588282
+
+CGPS tmp3 = x1 ( 1, 3 );
+cout << tmp3;
+ // [(51 30'0"N, 0 0'0"E) > (33 54'0"S, 151 12'0"E) > (37 25'18.84"N, 122 5'2.58"W)]
+
+istringstream tmp4 ( "[(51.5N, 0.0E) > (33.9S, 151.2E) > (37.42190N, 122.08405W) > (100.00N, 50.000W)]" );
 tmp4 >> x1;
-cout<<x1;
-cout<<tmp4.fail()<<endl;
+ // tmp4 . fail () = true
+cout << x1;
+ // [(50 6'16.5"N, 14 23'20.25"E) > (51 30'0"N, 0 0'0"E) > (33 54'0"S, 151 12'0"E) > (37 25'18.84"N, 122 5'2.58"W) > (0 0'0"N, 50 0'0"W)]
+
+CCoord tmp5 = x1 [ 1 ];
+cout << tmp5;
+ // (51 30'0"N, 0 0'0"E)
+
+
+//CCoord tmp6 = x1 [ -1 ];
+ // exception thrown
+
+//CCoord tmp7 = x1 [ 5 ];
+ // exception thrown
+
+CGPS tmp8 = x1 ( 2, 4 );
+cout << tmp8;
+ // [(33 54'0"S, 151 12'0"E) > (37 25'18.84"N, 122 5'2.58"W) > (0 0'0"N, 50 0'0"W)]
+
+//CGPS tmp9 = x1 ( 2, 5 );
+ // exception thrown
+
+//CGPS tmp10 = x1 ( 3, 1 );
+ // exception thrown
+
+istringstream tmp11 ( "[(51.5N, 0.0E) > (33.9S, 151.2E) > (37.42190N, 122.08405W) > (80.00N, 50.000W)]" );
+tmp11 >> x1;
+cout<<"---------------"<<endl;
+ // tmp11 . fail () = false
+cout << x1;
+ // [(51 30'0"N, 0 0'0"E) > (33 54'0"S, 151 12'0"E) > (37 25'18.84"N, 122 5'2.58"W) > (80 0'0"N, 50 0'0"W)]
+
+cout << x1;
+ // [(51 30'0"N, 0 0'0"E) > (33 54'0"S, 151 12'0"E) > (37 25'18.84"N, 122 5'2.58"W) > (80 0'0"N, 50 0'0"W)]
 return 0;
 
-}*/
+}
